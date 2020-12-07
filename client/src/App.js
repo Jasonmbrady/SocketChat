5876a694bebb8b6e7c97a16197ed332da058be16
Jason Brady <jasonmbrady42@gmail.com>
2020-12-07 02:41:15 -0800

    setup complete

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { useState, useEffect } from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to Socket.io chat!</h1>
+    </div>
+  );
+}
+
+export default App;
